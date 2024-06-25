5d8312b68be07c8c732ed9b8345a15ce052bb68a
Rameshwor Shrestha <r.sresta07@gmail.com>
2024-06-25 09:29:03 +0545

    maked the list multi select able

--- Assign2/lab06.java
+++ Assign2/lab06.java
@@ -1,3 +1,5 @@
+// to multi select: you need to select a item and press ctrl and cLick another item
+
 package Assign2;
 
 import javax.swing.JFrame;
@@ -5,15 +7,17 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
 
 public class lab06 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         JFrame frame = new JFrame("List");
         JPanel mainPanel = new JPanel();
-        
+
         JLabel lb1 = new JLabel("Select your favorite sports from the list: ");
-        String sport[] = {"Badminton", "Hockey", "Tennis", "Football", "Cricket", "Formula One","Rugby"};
-        JList <String> list = new JList<String>(sport);
+        String sport[] = { "Badminton", "Hockey", "Tennis", "Football", "Cricket", "Formula One", "Rugby" };
+        JList<String> list = new JList<>(sport);
+        list.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION); // Enable multiple selection
         JScrollPane scrollPane = new JScrollPane(list);
 
         mainPanel.add(lb1);
