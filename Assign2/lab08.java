a86d1ec2a825330f1c68e80d94eb7217ae9d5bc6
Rameshwor Shrestha <r.sresta07@gmail.com>
2024-06-27 08:45:17 +0545

    added some spacing

--- Assign2/lab08.java
+++ Assign2/lab08.java
@@ -7,23 +7,25 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
 public class lab08 {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Calculator");
-        JPanel mainPanel = new JPanel(new GridLayout(4,2));
-        
+        JPanel mainPanel = new JPanel(new GridLayout(4, 2));
+
         JLabel fNum = new JLabel("First Number");
+        fNum.setBorder(new EmptyBorder(0, 0, 0, 100)); // Add margin to the right
         JLabel sNum = new JLabel("Second Number");
         JLabel result = new JLabel("Result");
-        
+
         JButton bAdd = new JButton("+");
         JButton bSub = new JButton("-");
-        
+
         JTextField fValue = new JTextField();
         JTextField sValue = new JTextField();
         JTextField resValue = new JTextField();
-        
+
         mainPanel.add(fNum);
         mainPanel.add(fValue);
         mainPanel.add(sNum);
@@ -37,6 +39,6 @@ public class lab08 {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-        
+
     }
 }
