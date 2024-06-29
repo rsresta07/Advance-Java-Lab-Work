32b2ab0f503397e94534cab9777ec0842342a813
Rameshwor Shrestha <r.sresta07@gmail.com>
2024-06-29 12:55:18 +0545

    simple changes but everything is same

--- Assign2/lab11.java
+++ Assign2/lab11.java
@@ -5,49 +5,26 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
-public class lab11{
-
+public class lab11 {
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        frame.setTitle("MenusBar");
+        JFrame frame = new JFrame("MenusBar");
 
         JMenuBar menuBar = new JMenuBar();
+        String[] menuNames = { "File", "Edit", "Run", "Source", "Refactor", "Navigate", "Search", "Project", "Window",
+                "Help" };
+        for (String name : menuNames) {
+            menuBar.add(new JMenu(name));
+        }
 
-        JMenu fileMenu = new JMenu("File");
-        JMenu edit = new JMenu("Edit");
-        JMenu run = new JMenu("Run");
-        JMenu source = new JMenu("Source");
-        JMenu refactor = new JMenu("Refactor");
-        JMenu nav = new JMenu("Navigate");
-        JMenu search = new JMenu("Search");
-        JMenu project = new JMenu("Project");
-        JMenu window = new JMenu("Window");
-        JMenu help = new JMenu("Help");
+        JMenu project = (JMenu) menuBar.getMenu(7);
+        String[] projectItems = { "Open Project", "Close Project", "Build All", "Build Project" };
+        for (String item : projectItems) {
+            project.add(new JMenuItem(item));
+        }
 
-        JMenuItem openProject = new JMenuItem("Open Project");
-        JMenuItem closeProject = new JMenuItem("Close Project");
-        JMenuItem buildAll = new JMenuItem("Build All");
-        JMenuItem buildProject = new JMenuItem("Build Project");
-
-        project.add(openProject);
-        project.add(closeProject);
-        project.add(buildAll);
-        project.add(buildProject);
-
-        menuBar.add(fileMenu);
-        menuBar.add(edit);
-        menuBar.add(run);
-        menuBar.add(source);
-        menuBar.add(refactor);
-        menuBar.add(nav);
-        menuBar.add(search);
-        menuBar.add(project);
-        menuBar.add(window);
-        menuBar.add(help);
-        
         frame.setJMenuBar(menuBar);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600,600);
+        frame.setSize(600, 600);
         frame.setVisible(true);
     }
 }
