60b703c149ae50180f0c2211716a51fdda811b6a
Rameshwor Shrestha <r.sresta07@gmail.com>
2024-07-18 18:00:08 +0545

    left to do edit student data

--- Assign5/lab01/StudentManager.java
+++ Assign5/lab01/StudentManager.java
@@ -81,55 +81,7 @@ public class StudentManager extends JFrame {
     }
 
     private void editStudent() {
-        int selectedRow = studentTable.getSelectedRow();
-        if (selectedRow != -1) {
-            int studentId = (int) studentTable.getValueAt(selectedRow, 0);
-
-            JTextField nameField = new JTextField((String) studentTable.getValueAt(selectedRow, 1));
-            JComboBox<String> genderComboBox = new JComboBox<>(new String[] { "Male", "Female", "Others" });
-            genderComboBox.setSelectedItem(studentTable.getValueAt(selectedRow, 2));
-            JTextField birthdateField = new JTextField((String) studentTable.getValueAt(selectedRow, 3));
-            JTextField phoneField = new JTextField((String) studentTable.getValueAt(selectedRow, 4));
-            JTextField emailField = new JTextField((String) studentTable.getValueAt(selectedRow, 5));
-            JTextField addressField = new JTextField((String) studentTable.getValueAt(selectedRow, 6));
-
-            
-            JPanel editPanel = new JPanel(new GridLayout(6, 2));
-            editPanel.add(new JLabel("Name:"));
-            editPanel.add(nameField);
-            editPanel.add(new JLabel("Gender:"));
-            editPanel.add(genderComboBox);
-            editPanel.add(new JLabel("DOB:"));
-            editPanel.add(birthdateField);
-            editPanel.add(new JLabel("Phone:"));
-            editPanel.add(phoneField);
-            editPanel.add(new JLabel("Email:"));
-            editPanel.add(emailField);
-            editPanel.add(new JLabel("Address:"));
-            editPanel.add(addressField);
-
-            int result = JOptionPane.showConfirmDialog(null, editPanel, "Edit Student", JOptionPane.OK_CANCEL_OPTION);
-            if (result == JOptionPane.OK_OPTION) {
-                try (Connection conn = JDBCConnection.getConnection()) {
-                    String sql = "UPDATE tbl_student SET student_name = ?, gender = ?, birthdate = ?, phone = ?, email = ?, address = ? WHERE student_id = ?";
-                    PreparedStatement pstmt = conn.prepareStatement(sql);
-                    pstmt.setString(1, nameField.getText());
-                    pstmt.setString(2, (String) genderComboBox.getSelectedItem());
-                    pstmt.setString(3, birthdateField.getText());
-                    pstmt.setString(4, phoneField.getText());
-                    pstmt.setString(5, emailField.getText());
-                    pstmt.setString(6, addressField.getText());
-                    pstmt.setInt(7, studentId);
-                    pstmt.executeUpdate();
-                    JOptionPane.showMessageDialog(this, "Student details updated successfully");
-                    loadStudents(); // Refresh table data
-                } catch (SQLException ex) {
-                    ex.printStackTrace();
-                }
-            }
-        } else {
-            JOptionPane.showMessageDialog(this, "Please select a student to edit");
-        }
+        // TODO: edit student table information
     }
 
     private void deleteStudent() {
