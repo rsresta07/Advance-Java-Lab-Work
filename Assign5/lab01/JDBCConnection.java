9e85aa730d6a4ada8a27dce3f370137cc7722939
Rameshwor Shrestha <r.sresta07@gmail.com>
2024-07-18 13:35:53 +0545

    Connection file is renamed to JDBCConnection

--- Assign5/lab01/JDBCConnection.java
+++ Assign5/lab01/JDBCConnection.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectionE {
+public class JDBCConnection {
     public static Connection getConnection() throws SQLException {
         String url = "jdbc:mysql://localhost:3306/nccs_db";
         String user = "root";
